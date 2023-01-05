5f8b469a2270bbba15c3810b3bd1b06b25867b01
Kaly-7 <kaly.salas.7@gmail.com>
2023-01-05 20:32:09 +0100

    config

--- managemoney-service/managemoney-command-service/src/main/java/com/kaly7dev/managemoneycommandservice/controllers/TransactionCommandController.java
+++ managemoney-service/managemoney-command-service/src/main/java/com/kaly7dev/managemoneycommandservice/controllers/TransactionCommandController.java
@@ -6,13 +6,19 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
 
 @RestController
+@RefreshScope
 @Slf4j
 @AllArgsConstructor
 @RequestMapping("/transaction/commands")
@@ -20,20 +26,40 @@ public class TransactionCommandController {
     private CommandGateway commandGateway;
     private EventStore eventStore;
 
+    private String serverPort;
+    private String value;
+
+    @Autowired
+    public TransactionCommandController(@Value("${server.port}") String serverPort, @Value("${value}") String value ) {
+        this.serverPort = serverPort;
+        this.value = value;
+    }
+
+
     @PostMapping("/create")
     public CompletableFuture<String> newTransaction(@RequestBody TransactionResqDto resqDto){
-        CompletableFuture<String> response = commandGateway.send(new CreateTransactionCommand(
+        return commandGateway.send(new CreateTransactionCommand(
                 UUID.randomUUID().toString(),
                 resqDto.getDesc(),
                 resqDto.getAmount(),
                 resqDto.getMonth(),
                 resqDto.getWeek()
         ));
-        return response;
     }
 
     @GetMapping("/eventStore/{transId}")
     public Stream eventStore(@PathVariable String transId){
         return eventStore.readEvents(transId).asStream();
+    }
+
+
+
+    @GetMapping("/myconfig")
+    public Map<String, Object> myConfig(){
+        Map<String, Object> params = new HashMap< >();
+        params.put("server.port", serverPort);
+        params.put("value", value);
+        params.put("threadName", Thread.currentThread().getName());
+        return params;
     }
 }
