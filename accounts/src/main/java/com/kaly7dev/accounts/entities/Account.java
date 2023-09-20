ab3e31b26dfab037f60fdaf99c3594674311dd54
kaly-77 <kaly.salas.7@gmail.com>
2023-09-20 05:37:15 +0100

    Docker-compose databases

--- accounts/src/main/java/com/kaly7dev/accounts/entities/Account.java
+++ accounts/src/main/java/com/kaly7dev/accounts/entities/Account.java
@@ -7,7 +7,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.math.BigDecimal;
 import java.time.Instant;
 @Document(value = "accounts")
-//@Entity
 @Setter
 @Getter
 @AllArgsConstructor
