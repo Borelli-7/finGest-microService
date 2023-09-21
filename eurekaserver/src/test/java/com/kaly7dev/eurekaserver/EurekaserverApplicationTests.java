00b9f90cfa854a565b42e0d7d587e3b647817887
kaly-77 <kaly.salas.7@gmail.com>
2023-09-21 17:14:07 +0100

    first discovery set

---
+++ eurekaserver/src/test/java/com/kaly7dev/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaly7dev.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
